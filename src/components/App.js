d5730e218dff3b37eb62500e13ed61d308eeb8b6
ranjith1807 <v.ranjith9095490240@gmail.com>
2026-04-22 20:58:29 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,35 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
-
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+const projects = [
+  {
+    id: 1,
+    projectname: "abcde",
+    projectdescription: "sdfghjkl;",
+  },
+  {
+    id: 2,
+    projectname: "abchfde",
+    projectdescription: "sdfghjjhjsdkfslkjflskkmslkdmkl;",
+  },
+];
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
+    <div id="main" className="ns-wrapper">
+      {projects.map((project)=>( 
+        <div key={project.id} className="project-item">
+      <h1 data-ns-test="project-name">
+        {project.projectname}
+        </h1>
+      <h6 data-ns-test="project-description">
+        {project.projectdescription}
+        </h6>
+       </div>
+       ))
 
+      }
+      
+    </div>
+  );
+};
 
 export default App;
